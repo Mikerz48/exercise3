3ba5774a6778aa3094b3c50c878c57e6a5c24e61
Mikerz48 <michaelrenna48@gmail.com>
2023-06-03 17:54:55 +0200

    Changed the function

---
+++ exercise.js
@@ -0,0 +1,27 @@
+ //const sum = function (a, b) {
+ // return a + b;
+//}
+
+//const subtract = function (a, b) {
+  //return a - b;
+//}
+
+//const multiply = function (a, b) {
+  //return a * b;
+//}
+
+//const divide = function (a, b) {
+  //return a / b;
+//}
+
+//const log = function (value) {
+  //console.log(value);
+//} 
+
+const resultCalc = () => {
+  const result = ((2 + 4) * (5 + 2) - 2) / 5;
+  console.log(result);
+}
+
+resultCalc();
+
